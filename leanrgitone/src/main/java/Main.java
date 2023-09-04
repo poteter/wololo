b23e3d7fa081205ff65585c914afa096403077d1
poteter <eith2312@gmail.com>
2023-09-04 18:57:51 +0200

    added shipsflakes

--- leanrgitone/src/main/java/Main.java
+++ leanrgitone/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("wololo");
         System.out.println("woah");
+        System.out.println("shipsflakes");
     }
 }
